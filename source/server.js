3538b75445b6d4d6659cfd86ba108e2fdc088a07
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2022-09-23 17:00:03 +0100

    Added bedrock tests

--- source/server.js
+++ source/server.js
@@ -312,4 +312,25 @@ const httpServer = http.createServer(app);
 const PORT = process.env.PORT || 6060;
 httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
 
+const { Client } = require('raknet-native')
+const client = new Client('bedrock.peacefulvanilla.club', 19132, 'minecraft')
+console.log('listening for data');
+let timer = 
+
+client.on('pong', (data) => {
+      const msg = data.extra?.toString()
+      console.log('Decoded Buffer:')
+      console.log(msg);
+      console.log('OK')
+	  clearTimeout(timer)
+    });
+	console.log(client);
+	
+	setTimeout(function() {
+	timer = setTimeout(function() {
+    console.log('Offline')
+	}, 5000);
+    client.ping();
+	}, 10000)
+
 
