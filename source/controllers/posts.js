d531d7828a487c862eedabcca58e44fe400c504f
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2022-09-30 16:23:13 +0100

    Update posts.js

--- source/controllers/posts.js
+++ source/controllers/posts.js
@@ -145,7 +145,7 @@ enableSRV: true // SRV record lookup
 // port will default to 19132.
 util.statusBedrock('bedrock.peacefulvanilla.club', 19132, options)
     .then((result) => {
-return res.status(200).json(result)
+return res.status(200).json(JSON.parse(JSON.stringify(result, (_, v) => typeof v === 'bigint' ? v.toString() : v)))
 	})
     .catch((error) => {
 	console.log(error);
